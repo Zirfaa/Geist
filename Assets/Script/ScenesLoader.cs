43b3c294dab76e8814a5c51a3fce38364d764833
Zirfaa <zirlydaffa321@gmail.com>
2026-04-26 09:10:07 +0700

    add function ingame button + bg "belum rapi"

--- Assets/Script/ScenesLoader.cs
+++ Assets/Script/ScenesLoader.cs
@@ -3,35 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    // Buat helper private agar tidak perlu tulis ulang di setiap method
+    private void LoadScene(string sceneName)
+    {
+        Time.timeScale = 1f; // Reset timescale sebelum pindah scene
+        SceneManager.LoadScene(sceneName);
+    }
+
     public void GoToMainMenu()
     {
-        SceneManager.LoadScene("MainMenu");
+        LoadScene("MainMenu");
+    }
+    public void ReloadCurrentScene()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void GoToSelectLevel()
     {
         Debug.Log("Tombol Play ditekan");
-        SceneManager.LoadScene("SelectLevel");
+        LoadScene("SelectLevel");
     }
 
-    public void GoToLvl1()
-    {
-        SceneManager.LoadScene("Lv1");
-    }
-    public void GoToLvl2()
-    {
-        SceneManager.LoadScene("Lv2");
-    }
-    public void GoToLvl3()
-    {
-        SceneManager.LoadScene("Lv3");
-    }
-    public void GoToLvl4()
-    {
-        SceneManager.LoadScene("Lv4");
-    }
-    public void GoToLvl5()
-    {
-        SceneManager.LoadScene("Lv5");
-    }
+    public void GoToLvl1() => LoadScene("Lv1");
+    public void GoToLvl2() => LoadScene("Lv2");
+    public void GoToLvl3() => LoadScene("Lv3");
+    public void GoToLvl4() => LoadScene("Lv4");
+    public void GoToLvl5() => LoadScene("Lv5");
 }
