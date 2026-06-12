5a9b6bbd9ac9d4e3b8d9b32eb156c796c1ba25ab
Pramudya76 <pramudyarama76@gmail.com>
2026-06-13 03:12:17 +0700

    menambah script enemy di lv7 - lv10

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private int currentDirection = 0;
-    private int currentStep = 0;
+    [SerializeField]private int currentDirection = 0;
+    [SerializeField]private int currentStep = 1;
     public int maxStep;
     Vector3Int[] directions = new Vector3Int[]
     {
@@ -27,6 +27,16 @@ public class Enemy : MonoBehaviour
 
     IEnumerator MoveNextTile()
     {
+        currentStep++;
+        if(currentStep >= maxStep)
+        {
+            currentStep = 1;
+            currentDirection++;
+            if(currentDirection >= directions.Length)
+            {
+                currentDirection = 0;
+            }
+        }
         Vector3Int gridPos = GridManager.Instance.WorldToGrid(transform.position);
         Vector3Int nextGrid = gridPos + directions[currentDirection];
         Vector3 targetPos = GridManager.Instance.GridToWorld(nextGrid);
@@ -36,24 +46,14 @@ public class Enemy : MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, targetPos, 5 * Time.deltaTime);
             yield return null;
         }
-        currentStep++;
 
-        if(currentStep > maxStep)
-        {
-            currentStep = 0;
-            currentDirection++;
-            if(currentDirection >= directions.Length)
-            {
-                currentDirection = 0;
-            }
-        }
     }
 
     IEnumerator CallPatrol()
     {
         while(true)
         {
-            StartCoroutine(MoveNextTile());
+            yield return StartCoroutine(MoveNextTile());
             yield return new WaitForSeconds(1f);
         }
     }
